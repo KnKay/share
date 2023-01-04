2d84f0b8ceb827d81abaa4e1b160e973f4a1e5a2
KnKay <kay.richter@googlemail.com>
2023-01-04 17:06:21 +0100

    link in list working

--- frontend/src/components/AssetGroupsList.js
+++ frontend/src/components/AssetGroupsList.js
@@ -4,7 +4,10 @@ import { isLoggedIn } from "axios-jwt";
 import {axiosInstance} from "./Api";
 import {DataTable} from "primereact/datatable";
 import {Column} from "primereact/column";
+import React from 'react';
 
+import { Button } from 'primereact/button';
+import {Link} from 'react'
 /*
 At the moment this is a simple list.
 This should become a carousel or something more elegant in future.
@@ -24,11 +27,22 @@ function AssetGroupsList(){
 
     if (error) return "An error has occurred: " + error.message;
 
+    const actionBodyTemplate = (rowData) => {
+        return (
+            <React.Fragment>
+                    <a href={"/groups/"+rowData.id}>
+                <Button icon="pi pi-pencil" className="p-button-rounded p-button-success mr-2"  />
+                    </a>
+            </React.Fragment>
+        );
+    }
+
     return (
         <div className={"col mr-3"}>
             <DataTable value={data} stripedRows className={"p-1"}>
                 <Column field="name" header="Name" className={"p-1"} />
                 <Column field="description" header="Beschreibung" className={"p-1"} />
+                <Column body={actionBodyTemplate} exportable={false} className={"p-1"}></Column>
             </DataTable>
         </div>
 
