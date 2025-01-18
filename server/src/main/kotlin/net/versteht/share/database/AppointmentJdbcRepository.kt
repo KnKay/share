12fc088d840ee3cd03914a14a1a3982180a2675d
knkay <kay.richter@googlemail.com>
2025-01-18 22:56:37 +0100

    Added appointment. Need to be checked for datetime as this is string at the moment

--- server/src/main/kotlin/net/versteht/share/database/AppointmentJdbcRepository.kt
+++ server/src/main/kotlin/net/versteht/share/database/AppointmentJdbcRepository.kt
@@ -2,13 +2,16 @@ package net.versteht.share.database
 
 
 
-import io.ktor.server.routing.*
+import kotlinx.datetime.toKotlinLocalDateTime
 import net.versteht.share.objects.Appointment
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.deleteWhere
+import java.time.LocalDateTime
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 
 class  AppointmentJdbcRepository(database: Database) : CrudRepositoryInterface<Appointment> {
 
@@ -19,8 +22,14 @@ class  AppointmentJdbcRepository(database: Database) : CrudRepositoryInterface<A
         }
     }
 
-    override suspend fun create(t: Appointment): Appointment? {
-        TODO("Not yet implemented")
+    override suspend fun create(t: Appointment): Appointment?  = suspendTransaction {
+        DAOtoAppointment(AppointmentDAO.new{
+            item = ItemDAO.findById(t.item.id!!)!!
+            requester = UserDAO.findById(t.requester?.id!!)!!
+            startDate = LocalDateTime.parse(t.startDate)
+            endDate = LocalDateTime.parse(t.endDate)
+            confirmed = false
+        })
     }
 
     override suspend fun read(id: Int): Appointment?  = suspendTransaction {
@@ -37,12 +46,15 @@ class  AppointmentJdbcRepository(database: Database) : CrudRepositoryInterface<A
                 .map(::DAOtoAppointment)
     }
 
-    override suspend fun delete(t: Appointment): Boolean {
-        TODO("Not yet implemented")
+    override suspend fun delete(t: Appointment): Boolean = suspendTransaction {
+        1 == AppointmentTable.deleteWhere { id eq t.id }
     }
 
-    override suspend fun update(t: Appointment): Appointment {
-        TODO("Not yet implemented")
+    override suspend fun update(t: Appointment): Appointment = suspendTransaction {
+        var ret = AppointmentDAO.findByIdAndUpdate(t.id!!){
+            it.confirmed = t.confirmed
+        }
+        DAOtoAppointment(ret!!)
     }
 
 }
--- shared/src/commonMain/kotlin/net/versteht/share/objects/Appointment.kt
+++ shared/src/commonMain/kotlin/net/versteht/share/objects/Appointment.kt
@@ -1,13 +1,14 @@
 package net.versteht.share.objects
 
-import kotlinx.datetime.DateTimeUnit
-import kotlinx.datetime.LocalDateTime
+
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Appointment(
     val item: Item,
-    val startDate: LocalDateTime?,
-    val endDate: LocalDateTime?,
-    val confirmed: Boolean,
+    val startDate: String,
+    val endDate: String,
+    var confirmed: Boolean,
+    val requester: User?,
+    val id: Int? = null,
 )
