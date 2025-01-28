ed1fa6e444b8fdeb8237865c92b7e9d2fec9bade
knkay <kay.richter@googlemail.com>
2025-01-28 21:12:57 +0100

    Added tokens

--- shared/src/commonMain/kotlin/net/versteht/share/client/ShareClient.kt
+++ shared/src/commonMain/kotlin/net/versteht/share/client/ShareClient.kt
@@ -2,6 +2,7 @@ package net.versteht.share.client
 
 import kotlinx.coroutines.*
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.auth.Auth
 import io.ktor.client.plugins.auth.providers.BearerTokens
@@ -14,32 +15,41 @@ import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.serialization.kotlinx.json.*
-import io.ktor.util.Hash
-import net.versteht.share.client.repositories.CategoryRepository
+import net.versteht.share.client.repositories.*
 import net.versteht.share.objects.Login
 
 // Our share client should encapsulate our application data...
 class ShareClient(client: HttpClient) {
-    val client = client
     val categoryRepository = CategoryRepository(client,"categories")
+    val itemRepository = ItemRepository(client,"items")
 
     companion object Factory{
         suspend fun createInstance(baseUrl: String, email: String, password: String, engine: HttpClientEngine): ShareClient  {
-            val httpClient = HttpClient(engine) {
+            var httpClient = HttpClient(engine) {
                 install(ContentNegotiation) {
                     json()
                 }
             }
-            httpClient.post("${baseUrl}/login"){
+            val token = httpClient.post("${baseUrl}/login"){
                 header(
                     HttpHeaders.ContentType,
                     ContentType.Application.Json
                 )
                 setBody(Login(email = email, password = password))
+            }.body<HashMap<String, String>>()
+            httpClient = HttpClient(engine) {
+                install(ContentNegotiation) {
+                    json()
+                }
+                install(Auth){
+                    bearer{
+                        loadTokens {
+                            BearerTokens(token["token"]!!, token["another"])
+                        }
+                    }
+                }
             }
-
             return ShareClient(httpClient)
-
         }
     }
 }
